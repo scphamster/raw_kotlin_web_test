7fcb1be1b9256e65a050a6ee7eb7e48e668063f5
scpha <scp.hamster@gmail.com>
2023-03-19 19:29:04 +0100

    initial

---
+++ src/jvmMain/kotlin/com.github.scphamster.application/Server.kt
@@ -0,0 +1,38 @@
+package com.github.scphamster.application
+
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.*
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.html.*
+import io.ktor.server.http.content.*
+import io.ktor.server.netty.Netty
+import io.ktor.server.routing.*
+import kotlinx.html.*
+
+fun HTML.index() {
+    head {
+        title("Hello from Ktor!")
+    }
+    body {
+        div {
+            +"Hello from Ktor"
+        }
+        div {
+            id = "root"
+        }
+        script(src = "/static/kotlin_multi_demo.js") {}
+    }
+}
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        routing {
+            get("/") {
+                call.respondHtml(HttpStatusCode.OK, HTML::index)
+            }
+            static("/static") {
+                resources()
+            }
+        }
+    }.start(wait = true)
+}
